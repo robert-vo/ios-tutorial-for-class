143701019d49614b7ba7a2894b4e335f55d91069
robert-vo <robertvo79@gmail.com>
2015-09-22 16:09:58 -0500

    Added user object.

---
+++ TutorialForUIView/User.h
@@ -0,0 +1,18 @@
+//
+//  User.h
+//  TutorialForUIView
+//
+//  Created by Robert Vo on 9/22/15.
+//  Copyright © 2015 RobertVo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property NSString *FirstName;
+@property NSString *LastName;
+@property NSString *Email;
+@property NSString *Password;
+
+@end
---
+++ TutorialForUIView/User.m
@@ -0,0 +1,15 @@
+//
+//  User.m
+//  TutorialForUIView
+//
+//  Created by Robert Vo on 9/22/15.
+//  Copyright © 2015 RobertVo. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@synthesize FirstName, LastName, Email, Password;
+
+@end
