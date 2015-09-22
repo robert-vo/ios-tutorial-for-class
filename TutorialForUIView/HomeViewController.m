e8287918de1e10e0b56021356adba7f9bc394a3a
robert-vo <robertvo79@gmail.com>
2015-09-22 16:34:00 -0500

    Implemented the prepareForSegue action. HomeController will go to LoggedInController.

--- TutorialForUIView/HomeViewController.m
+++ TutorialForUIView/HomeViewController.m
@@ -27,15 +27,14 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    HomeViewController* vc = [segue destinationViewController];
+    //vc.EmailTextField.text = EmailTextField.text;
+    
+    //vc.bookData = selectedBook;
 }
-*/
+
 
 - (IBAction)FirstNameFieldTouched:(id)sender {
     FirstNameTextField.text = @"";
@@ -66,6 +65,9 @@
     user.Email = EmailTextField.text;
     user.Password = PasswordTextField.text;
     NSLog(@"the values are...");
+    
+    [self performSegueWithIdentifier:@"LoggedInVC" sender:nil];
+
 }
 
 - (IBAction)ResetButtonTouched:(id)sender {
