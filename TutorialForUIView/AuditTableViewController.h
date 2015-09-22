180ed701685927ac4817570461b895202eb4f6ac
robert-vo <robertvo79@gmail.com>
2015-09-22 18:28:16 -0500

    Added Audit Table View Controller.

---
+++ TutorialForUIView/AuditTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AuditTableViewController.h
+//  TutorialForUIView
+//
+//  Created by Robert Vo on 9/22/15.
+//  Copyright © 2015 RobertVo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuditTableViewController : UITableViewController
+
+@end
---
+++ TutorialForUIView/AuditTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  AuditTableViewController.m
+//  TutorialForUIView
+//
+//  Created by Robert Vo on 9/22/15.
+//  Copyright © 2015 RobertVo. All rights reserved.
+//
+
+#import "AuditTableViewController.h"
+
+@implementation AuditTableViewController
+
+@end
