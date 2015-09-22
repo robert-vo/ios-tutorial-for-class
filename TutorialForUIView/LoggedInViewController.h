04851677181bc7671873f7a0e8136b5fd4adb2c1
robert-vo <robertvo79@gmail.com>
2015-09-22 15:27:09 -0500

    Added home/loggedin controllers.

---
+++ TutorialForUIView/LoggedInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoggedInViewController.h
+//  TutorialForUIView
+//
+//  Created by Robert Vo on 9/22/15.
+//  Copyright © 2015 RobertVo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoggedInViewController : UIViewController
+
+@end
