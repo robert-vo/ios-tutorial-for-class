ef165fb7f6bf27d127b9f8109ae0eb7be94b9ccc
robert-vo <robertvo79@gmail.com>
2015-11-19 04:42:22 -0600

    Added new HomeViewController.

---
+++ TutorialForUIView/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  TutorialForUIView
+//
+//  Created by Robert Vo on 11/19/15.
+//  Copyright © 2015 RobertVo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *EmailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *PasswordTextField;
+- (IBAction)LoginButton:(id)sender;
+
+@end
